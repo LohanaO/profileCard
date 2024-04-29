8e99386d03e8049ff071f0432e21de8a078289a2
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2024-04-29 23:14:51 +0200

    mod img

--- src/componets/Card.tsx
+++ src/componets/Card.tsx
@@ -3,7 +3,7 @@ const Card = () => {
     <div className="text-white bg-[#242323] w-[386px] h-[439px] flex flex-col justify-center items-center gap-2 p-10">
         <div className="flex flex-col justify-center items-center gap-3">
       <div className=" bg-white  rounded-full mt-7 mb-2">
-        <img src="./src/assets/img-Perfil.webp" alt="img-perfil" className=" rounded-full w-[146px] h-[146px]" style={{ objectFit: 'cover', objectPosition: 'center 5%' }} />
+        <img src="./img-Perfil.webp" alt="img-perfil" className=" rounded-full w-[146px] h-[146px]" style={{ objectFit: 'cover', objectPosition: 'center 5%' }} />
       </div>
       <h2 className="text-[#C2C2C2] text-lg font-bold font-montserrat">Lohana Orellano</h2>
       <div className="bg-[#1E1D1D] p-1 px-2 rounded-2xl flex justify-center items-center ">
